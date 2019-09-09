6cbce8799e60e54e6fd3ac9661017175ffeb6a20
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-09 11:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+const app = "I don't do anuthing."
 
-// Add your functions and code here
+function restoreKittens(){
+  return ["Milo", "Otis", "Garfield"]
+};
+
+function destructivelyAppendKitten(name){
+  window.kittens = restoreKittens()
+  window.kittens.push(name)
+  return window.kittens
+};
+
+function destructivelyPrependKittens(name){
+  window.kitten = restoreKittens()
+  window.kittens.unshift(name)
+  return window.kittens
+};
+
+function destrutevlyRemoveLastKitten(name){
+  window.kitten = restoreKittens()
+  window.kittens.pop(name)
+  return window.kittens
+};
+
+function destructivelyRemoveFirstKitten(name){
+  window.kitten = restoreKittens()
+  window.kittens.shift(name)
+  return window.kittens
+};
+
+
+
